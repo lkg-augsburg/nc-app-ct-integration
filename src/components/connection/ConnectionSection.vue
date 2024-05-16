e1a6a24af0944d84d533b726f49278ae4f5437da
Søren Liebich <soeren.Liebich@gmail.com>
2024-05-16 19:24:55 +0200

    feat: :sparkles: add connection response messages

--- src/components/connection/ConnectionSection.vue
+++ src/components/connection/ConnectionSection.vue
@@ -17,7 +17,7 @@
       Test Connection
     </NcButton>
     <NcButton :wide="false"
-      :disabled="isConnectionTested === false && connectionOk === false"
+      :disabled="isConnectionTested === false || error !== null"
       @click="saveCredentials">
       <template #icon>
         <ContentSaveIcon :size="20" />
@@ -25,66 +25,45 @@
       Save credentials
     </NcButton>
   </div>
-  <template v-if="isConnectionTested === true && connectionOk !== true">
-    <NcNoteCard type="warning">
-      <p>{{ error.status }} {{ error.statusText }}</p>
-      <p>{{ error.message }}</p>
-    </NcNoteCard>
-    <div>
-      <!-- eslint-disable-next-line vue/no-v-html -->
-      <span v-html="error.errHtml" />
-    </div>
-  </template>
-  <NcNoteCard v-if="isConnectionTested === true && connectionOk === true"
-    type="success">
-    <p>Connection successfully established</p>
-    <!-- For some reason grid-cols-[100px_minmax(0px,_1fr)] doesn't work-->
-    <div class="grid gap-1" style="grid-template-columns: 100px 1fr;">
-      <div>Id</div>
-      <div>{{ whoami.id }}</div>
-      <div>Mail</div>
-      <div>{{ whoami.email }}</div>
-      <div>Username</div>
-      <div>{{ whoami.cmsUserId }}</div>
-    </div>
-    <div>
-      <a :href="`https://lkg-augsburg.church.tools/?q=churchdb#PersonView/searchEntry:%23${whoami.id}`"
-        class="flex hover:font-bold">
-        <OpenInNewIcon :size="20" class="mr-1" />
-        Open in ChurchTools
-      </a>
-    </div>
-  </NcNoteCard>
+  <ConnectionErrorCard v-if="(isConnectionTested === true || credentialsSaved === true) && error !== null"
+    :status="error.status"
+    :status-text="error.statusText"
+    :message="error.message"
+    :err-html="error.errHtml" />
+  <ConnectionSuccessCard v-if="isConnectionTested === true && error === null"
+    :id="whoami.id"
+    :url="state.ctUrl"
+    :email="whoami.email"
+    :user="whoami.cmsUserId" />
+  <CredentialsSaveSuccessCard v-if="credentialsSaved === true && error === null"
+    text="ChurchTools credentials successfully saved." />
 </ConfigSection>
 </template>
 <script>
-import { churchtoolsClient, activateLogging } from '@churchtools/churchtools-client'
 import { loadState } from '@nextcloud/initial-state'
 import { generateUrl } from '@nextcloud/router'
 import axios from '@nextcloud/axios'
 import NcButton from '@nextcloud/vue/dist/Components/NcButton.js'
-import NcNoteCard from '@nextcloud/vue/dist/Components/NcNoteCard.js'
 import ConnectionIcon from 'vue-material-design-icons/Connection.vue'
-import OpenInNewIcon from 'vue-material-design-icons/OpenInNew.vue'
 import ContentSaveIcon from 'vue-material-design-icons/ContentSave.vue'
 import ConfigSection from '../forms/ConfigSection.vue'
 import InputField from '../forms/InputField.vue'
+import ConnectionErrorCard from './ConnectionErrorCard.vue'
+import ConnectionSuccessCard from './ConnectionSuccessCard.vue'
+import CredentialsSaveSuccessCard from './CredentialsSaveSuccessCard.vue'
 
-activateLogging()
-churchtoolsClient.setBaseUrl('https://lkg-augsburg.church.tools')
-
-const baseURL = generateUrl('/apps/churchtoolsintegration/api')
-axios.defaults.baseURL = baseURL
+axios.defaults.baseURL = generateUrl('/apps/churchtoolsintegration/api')
 
 export default {
   name: 'ConnectionSection',
   components: {
     ConfigSection,
+    ConnectionErrorCard,
+    ConnectionSuccessCard,
+    CredentialsSaveSuccessCard,
     InputField,
     NcButton,
-    NcNoteCard,
     ConnectionIcon,
-    OpenInNewIcon,
     ContentSaveIcon,
   },
   data: () => ({
@@ -92,16 +71,35 @@ export default {
     state: loadState('churchtoolsintegration', 'ct-connection'),
     whoami: null,
     isConnectionTested: false,
-    connectionOk: false,
     error: null,
+    credentialsSaved: false,
   }),
   methods: {
     async saveCredentials() {
-      await axios.post('ct-credentials', {
-        url: this.state.ctUrl,
-        token: this.state.ctToken,
-      })
+      this.isConnectionTested = false
+      this.error = null
 
+      try {
+        await axios.post('ct-credentials', {
+          url: this.state.ctUrl,
+          token: this.state.ctToken,
+        })
+      } catch (e) {
+        console.error(e)
+        const parser = new DOMParser()
+        const errDoc = parser.parseFromString(e.response.data, 'text/html')
+        const errDocMsg = errDoc.querySelector('.guest-box.wide')
+        this.error = {
+          error: e,
+          errHtml: errDocMsg.outerHTML,
+          code: e.code,
+          message: e.message,
+          status: e.response.status,
+          statusText: e.response.statusText,
+        }
+      } finally {
+        this.credentialsSaved = true
+      }
     },
     async testLogin() {
       this.isConnectionTested = false
@@ -116,8 +114,6 @@ export default {
 
         const { id, email, cmsUserId } = whoAmIResp.data.data
         this.whoami = { id, email, cmsUserId }
-
-        this.connectionOk = true
       } catch (e) {
         console.error(e)
 
@@ -125,7 +121,6 @@ export default {
         const errDoc = parser.parseFromString(e.response.data, 'text/html')
         const errDocMsg = errDoc.querySelector('.guest-box.wide')
 
-        this.connectionOk = false
         this.error = {
           error: e,
           errHtml: errDocMsg.outerHTML,
---
+++ src/components/connection/CredentialsSaveSuccessCard.vue
@@ -0,0 +1,16 @@
+<template>
+<NcNoteCard type="success" />
+</template>
+<script>
+import NcNoteCard from '@nextcloud/vue/dist/Components/NcNoteCard.js'
+
+export default {
+  name: 'CredentialsSaveSuccessCard',
+  components: {
+    NcNoteCard,
+  },
+  props: {
+    text: { type: String, required: true },
+  },
+}
+</script>
